459125ec7cf90b2e7555d55a4712fc518253a3af
danny <danny@pop-os.localdomain>
2020-12-15 17:53:40 -0500

    export more classes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,3 @@
-## 0.0.3
+## 0.0.4
 
-- added a "mock" mode
-
-`StudentVueClient(mock: true)`
+- export more classes
--- lib/studentvueclient.dart
+++ lib/studentvueclient.dart
@@ -4,5 +4,7 @@
 library studentvueclient;
 
 export 'src/studentvueclient_base.dart';
+export 'src/studentgradedata.dart';
+export 'src/studentdata.dart';
 
 // TODO: Export any libraries intended for clients of this package.
